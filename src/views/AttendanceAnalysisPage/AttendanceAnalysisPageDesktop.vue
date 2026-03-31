8bf32c0ebdd38bed037a1f3578e56b2763a12962
ZhengEnCi <zheng_enci@qq.com>
2026-03-31 20:51:58 +0800

    添加考勤分析页面顶部栏

---
+++ src/views/AttendanceAnalysisPage/AttendanceAnalysisPageDesktop.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goBack = () => {
+	router.push('/navigation-desktop')
+}
+</script>
+
+<template>
+	<div class="attendance-analysis-page">
+		<div class="header">
+			<div class="header-left">
+				<img
+					src="@/assets/AiWorkShop_icon.png"
+					alt="Logo"
+					class="logo"
+				/>
+				<div class="title-section">
+					<h1>考勤分析</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+@import './css/AttendanceAnalysisPageDesktop.css';
+</style>
