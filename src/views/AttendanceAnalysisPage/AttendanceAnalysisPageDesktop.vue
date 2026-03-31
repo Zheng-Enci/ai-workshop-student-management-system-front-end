6d2ce457848eaf2974631795745f17478a63e2da
ZhengEnCi <zheng_enci@qq.com>
2026-03-31 21:03:32 +0800

    实现考勤分析页面主题切换

--- src/views/AttendanceAnalysisPage/AttendanceAnalysisPageDesktop.vue
+++ src/views/AttendanceAnalysisPage/AttendanceAnalysisPageDesktop.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { useRouter } from 'vue-router'
+import { useThemeStore } from '@/stores/theme'
 
 const router = useRouter()
+const themeStore = useThemeStore()
+const { toggleTheme } = themeStore
 
 const goBack = () => {
 	router.push('/navigation-desktop')
@@ -16,6 +19,8 @@ const goBack = () => {
 					src="@/assets/AiWorkShop_icon.png"
 					alt="Logo"
 					class="header-logo"
+					title="切换主题模式"
+					@click="toggleTheme"
 				/>
 				<div class="header-title-section">
 					<h1>考勤分析</h1>
@@ -27,4 +32,5 @@ const goBack = () => {
 
 <style scoped>
 @import './css/desktop/header.css';
+@import './css/desktop/page.css';
 </style>
