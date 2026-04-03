8ec5d8c3d5eb184ae595c78d2cfe34bbc2284382
ZhengEnCi <zheng_enci@qq.com>
2026-04-03 12:40:41 +0800

    fix: 回退dark-theme检测

--- src/views/AttendanceAnalysisPage/ts/attendanceTrendChart.ts
+++ src/views/AttendanceAnalysisPage/ts/attendanceTrendChart.ts
@@ -125,7 +125,7 @@ class AttendanceTrendChart {
 	}
 
 	private getDarkMode(): boolean {
-		return document.documentElement.classList.contains('dark') || document.documentElement.classList.contains('dark-theme')
+		return document.documentElement.classList.contains('dark')
 	}
 
 	setOption(dates: string[], values: number[]) {
