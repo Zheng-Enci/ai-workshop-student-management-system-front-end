55e1a70959cda6fcc5c0cfa2e36c83955b816ba1
郑恩赐 <3847632992@qq.com>
2026-04-06 15:18:25 +0800

    feat(profile): 导入用户信息区域CSS文件
    在Vue组件中导入用户信息区域样式文件,使样式生效

--- src/views/ProfilePage/ProfilePageDesktop.vue
+++ src/views/ProfilePage/ProfilePageDesktop.vue
@@ -41,6 +41,7 @@ import 'element-plus/theme-chalk/base.css'
 import { getMyAttendanceCount } from '@/api/attendance'
 import '@/views/ProfilePage/css/desktop/profile-page-desktop-header.css'
 import '@/views/ProfilePage/css/desktop/profile-page-desktop-avatar.css'
+import '@/views/ProfilePage/css/desktop/profile-page-desktop-user-info.css'
 import {
 	getStudentProfile,
 	updateStudentInfo,
