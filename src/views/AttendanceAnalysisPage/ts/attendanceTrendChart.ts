5a71fb4c5adc365982028ae8c9262b0b32051b7f
郑恩赐 <3847632992@qq.com>
2026-04-06 13:32:52 +0800

    feat: 趋势线宽度改回2

--- src/views/AttendanceAnalysisPage/ts/attendanceTrendChart.ts
+++ src/views/AttendanceAnalysisPage/ts/attendanceTrendChart.ts
@@ -253,7 +253,7 @@ class AttendanceTrendChart {
 					type: 'line',
 					data: trendLineValues,
 					smooth: false,
-					lineStyle: { color: this.getDarkMode() ? '#52c41a' : '#389e0d', width: 1 },
+					lineStyle: { color: this.getDarkMode() ? '#52c41a' : '#389e0d', width: 2 },
 					itemStyle: { color: this.getDarkMode() ? '#52c41a' : '#389e0d' },
 					symbol: 'none'
 				}
