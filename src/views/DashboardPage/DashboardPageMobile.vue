9b622c3db5ba74aa5455ba6a7920f405c915362b
郑恩赐 <3847632992@qq.com>
2026-04-06 12:50:03 +0800

    fix: 导入更多下拉框相关样式文件

--- src/views/DashboardPage/DashboardPageMobile.vue
+++ src/views/DashboardPage/DashboardPageMobile.vue
@@ -22,6 +22,9 @@ import { useRouter } from 'vue-router'
 import 'echarts-wordcloud'
 import 'element-plus/theme-chalk/el-select.css'
 import 'element-plus/theme-chalk/el-option.css'
+import 'element-plus/theme-chalk/el-input.css'
+import 'element-plus/theme-chalk/base.css'
+import 'element-plus/theme-chalk/el-icon.css'
 
 // 注册需要的组件
 echarts.use([
