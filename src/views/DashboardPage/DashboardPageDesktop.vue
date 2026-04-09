d8e38207690137925170807503aca27856f03258
ZhengEnCi <zheng_enci@qq.com>
2026-04-09 13:05:24 +0800

    style: 调整动画时长为1秒

--- src/views/DashboardPage/DashboardPageDesktop.vue
+++ src/views/DashboardPage/DashboardPageDesktop.vue
@@ -581,7 +581,7 @@ const initAttendanceChart = data => {
 				type: 'bar', // 柱状图类型
 				data: sortedData.map(item => item.attendanceCount), // 签到数
 				barWidth: '60%', // 柱子宽度
-				animationDuration: 500, // 动画时长0.5秒
+				animationDuration: 1000, // 动画时长1秒
 				animationEasing: 'cubicOut', // 缓动函数
 				universalTransition: true, // 启用通用过渡动画
 				itemStyle: {
