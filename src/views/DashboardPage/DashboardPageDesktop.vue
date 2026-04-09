11c28de7d0670d2bf5af023e206f953ae463e515
ZhengEnCi <zheng_enci@qq.com>
2026-04-09 16:00:24 +0800

    fix(dashboard): add missing icon imports to DashboardPageDesktop.vue
    - add Calendar, Clock, User, Setting, Star, Avatar icon imports
    - fix template rendering for dashboard statistics

--- src/views/DashboardPage/DashboardPageDesktop.vue
+++ src/views/DashboardPage/DashboardPageDesktop.vue
@@ -10,7 +10,7 @@ import { ElButton, ElIcon, ElProgress } from 'element-plus'
 import 'element-plus/theme-chalk/el-button.css'
 import 'element-plus/theme-chalk/el-icon.css'
 import 'element-plus/theme-chalk/el-progress.css'
-import { ArrowLeft } from '@element-plus/icons-vue'
+import { ArrowLeft, Calendar, Clock, User, Setting, Star, Avatar } from '@element-plus/icons-vue'
 import DashboardPageDesktopClass from './desktop/ts/DashboardPageDesktop'
 
 const dashboardPageDesktop = new DashboardPageDesktopClass()
