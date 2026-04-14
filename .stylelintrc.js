443600669769e6cd336024fa6d4ef864352e966e
ZhengEnCi <zheng_enci@qq.com>
2026-04-14 15:39:30 +0800

    chore(stylelint): add root stylelint config to reference code-quality config

---
+++ .stylelintrc.js
@@ -0,0 +1,5 @@
+// Stylelint 配置文件
+// 引用 code-quality 目录下的统一配置
+// Stylelint config file - references unified config in code-quality directory
+
+module.exports = require('./code-quality/code-quality-config/.stylelintrc.js')
