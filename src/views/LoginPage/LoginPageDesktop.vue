032934cdb128f870251a1147c86d23e6e704e6c0
ZhengEnCi <zheng_enci@qq.com>
2026-04-14 15:33:39 +0800

    refactor(LoginPageDesktop): update desktop login page component

--- src/views/LoginPage/LoginPageDesktop.vue
+++ src/views/LoginPage/LoginPageDesktop.vue
@@ -8,7 +8,7 @@
 
 // ===================== 第三方依赖导入 =====================
 import { User, Lock, House } from '@element-plus/icons-vue'
-import { ElMessage, ElButton, ElIcon, ElInput, ElForm, ElFormItem, ElCheckbox } from 'element-plus'
+import {ElButton, ElIcon, ElInput, ElForm, ElFormItem, ElCheckbox } from 'element-plus'
 import 'element-plus/theme-chalk/el-form.css'
 import 'element-plus/theme-chalk/el-form-item.css'
 import 'element-plus/theme-chalk/el-input.css'
@@ -78,7 +78,8 @@ const fetchStudentAvatar = loginPage.fetchStudentAvatar.bind(loginPage)
 							type="primary"
 							:icon="House"
 							circle
-							@click="goToHome"/>
+							@click="goToHome"
+						/>
 					</div>
 					<!-- 左侧内容区域，使用 img 标签显示成员合照 -->
 					<img
